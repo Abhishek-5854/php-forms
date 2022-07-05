75f86a85a34e12c9ecd29f92b4db505aff69a6bb
saurav00017 <sauravwagh2079@gmail.com>
2022-07-05 20:20:08 +0530

    demo_form6

---
+++ app/Industry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Industry extends Model
+{
+    //
+    protected $table = "fk_temp_free_trial_user_industry_master1_idx";
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/demo-form','enquiryies@index');
+Route::post('/send','enquiryies@store');
