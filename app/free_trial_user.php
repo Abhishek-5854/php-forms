28ab1ac836f4fd5c3f15ddef20ab611ec8bb8d77
Parameswaran <97391102+Vivek603@users.noreply.github.com>
2022-07-06 13:28:05 +0530

    Create free_trial_user.php

---
+++ app/free_trial_user.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class free_trial_user extends Model
+{protected $table = 'free_trial_user';
+    public $timestamps = false;
+    
+        
+    
+    
+    use HasFactory;
+}
