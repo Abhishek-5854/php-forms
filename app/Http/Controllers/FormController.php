67fc2c9f31fb1b6c34949823af08ef29b81874f9
Siddheshk02 <khandagalesid02@gmail.com>
2022-07-12 19:50:22 +0530

    request updated

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Form;
 use DB;
+use Crypt;
 
 class FormController extends Controller {
     public function show(){
@@ -22,14 +23,17 @@ class FormController extends Controller {
         $contact_number = request("phone");
         $encrypted_contact_no = encrypt($request->phone);
         $username = request("email");
+        $username_shorten = substr(($request->email),0,5).''.'************';
         $scheduled_date = request("demo_date");
         $scheduled_time_slot_id = request("time_slot");
+        $client_browser = parse_url( $_SERVER[ 'REQUEST_URI' ], PHP_URL_PATH );
         $client_ip = request()->ip();
-        $client_browser = request()->header('User-Agent');
+        $enquiry_from_page = request()->header('User-Agent');
         //$industry_name = request("industry");
         //$industry_id = DB::select("call ViewIndustryID('$industry_name')");
         $industry_id = request("industry");
-        DB::select('CALL guided_demo(?,?,?,?,?,?,?,?,?,?)',array("$full_name", "$company_name", "$contact_number", "$encrypted_contact_no", "$username", "$scheduled_date", "$scheduled_time_slot_id", "$client_ip", "$client_browser", "$industry_id"));
+        $username_encrypt = Crypt::encrypt( $request->email);
+        DB::select('CALL guided_demo(?,?,?,?,?,?,?,?,?,?,?,?,?)',array("$full_name", "$company_name", "$contact_number", "$encrypted_contact_no", "$username", "$username_shorten", "$scheduled_date", "$scheduled_time_slot_id", "$enquiry_from_page", "$client_ip", "$client_browser", "$industry_id", "$username_encrypt"));
         
         $request->validate([
             'demo_date' => 'required',
